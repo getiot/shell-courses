3fcc51e72abb94631c44bc24e2e860dc61fa8e41
luhuadong <luhuadong@163.com>
2021-05-31 14:33:35 +0800

    add first.sh

---
+++ first/first.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+################################################
+# Script: first.sh
+# Author: GetIoT
+# Date: 2021-05-20
+# Description: This is your first shell script 
+#              which says Hello to you.
+# Usage: ./first.sh
+################################################
+
+echo "What is your name?"
+read NAME
+echo "Hello, $NAME!"
